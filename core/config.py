3ac256938485af5eff1d8780200ebbddf1ad9c58
Mourcat <vybornovs@rambler.ru>
2024-12-15 22:17:35 +0300

    Found database and config meters

--- core/config.py
+++ core/config.py
@@ -1,15 +1,14 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseConfig(BaseModel):
-    db_url: PostgresDsn
+    url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
-    
 
 
 class RunConfig(BaseModel):
@@ -22,9 +21,17 @@ class ApiPrefixConfig(BaseModel):
     
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='APP-CONFIG__',
+    )
     runner: RunConfig = RunConfig()
     api: ApiPrefixConfig = ApiPrefixConfig()
-    db: DatabaseConfig = DatabaseConfig()
+    db: DatabaseConfig
+    
 
 
 settings = Settings()
+print(settings.db.url)
--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
 
-app = FastAPI()
-app.include_router(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # application startup
+    yield
+    
+    # application shutdown
+    print("Application shutdown")
+    await db_helper.dispose() 
+
+
+
+main_app = FastAPI(lifespan=lifespan)
+
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
 
 
+
 if __name__ == "__main__":
     uvicorn.run(
-        app="main:app",
+        app="main:main_app",
         host=settings.runner.host,
         port=settings.runner.port,
         reload=True,
