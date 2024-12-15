3bf0fd97e50d49fbb75e6c01bf24dc29f491852a
Mourcat <vybornovs@rambler.ru>
2024-12-15 23:32:01 +0300

    first migrations

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
+
+from datetime import datetime
+
 from .base import Base
 
 
@@ -7,3 +10,4 @@ class User(Base):
     last_name: Mapped[str] = mapped_column()
     father_name: Mapped[str] = mapped_column()
     service_id: Mapped[int] = mapped_column(unique=True)
+    birth_date: Mapped[datetime] = mapped_column()
