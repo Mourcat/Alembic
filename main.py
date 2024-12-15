f8da5fa96cd729c2208a546629d63eb2a2f230b9
Mourcat <vybornovs@rambler.ru>
2024-12-15 22:42:07 +0300

    install alembic and black

--- main.py
+++ main.py
@@ -18,14 +18,12 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose() 
 
 
-
 main_app = FastAPI(lifespan=lifespan)
 
 main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
-
 
 
 if __name__ == "__main__":
